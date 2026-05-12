c8a92cc8d004e03dc61a8c14df70d9b0c25c1761
Oleg M <olegminkin@MacBook-Pro-Oleg.local>
2026-05-12 14:48:34 -0300

    feat: configure next-pwa service worker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: { maxEntries: 4, maxAgeSeconds: 365 * 24 * 60 * 60 },
+      },
+    },
+    {
+      urlPattern: /\/api\//,
+      handler: 'NetworkOnly',
+    },
+    {
+      urlPattern: /.*/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'app-shell',
+        expiration: { maxEntries: 32, maxAgeSeconds: 24 * 60 * 60 },
+        networkTimeoutSeconds: 10,
+      },
+    },
+  ],
+}) as (config: NextConfig) => NextConfig
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  turbopack: {},
+}
 
-export default nextConfig;
+export default withPWA(nextConfig)
